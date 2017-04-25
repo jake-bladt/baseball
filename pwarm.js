7fa177a39448ef43d9622613f0f4571b2b207514
Jake Bladt <jake@ambulnz.com>
2017-04-25 13:47:07 -0400

    assign pitches

--- pwarm.js
+++ pwarm.js
@@ -3,4 +3,5 @@ let pitcher = require('./lib/pitcher');
 
 let bobSmith = player('Bob', 'Smith');
 pitcher(bobSmith);
-console.log(`${bobSmith.toString()} is taking the field.`)
+console.log(`${bobSmith.toString()} is taking the field.`);
+console.log(`${bobSmith.firstName} has ${bobSmith.pitchesAvailable.map(p => p.pitchName)}`);
