644198866ef035bcd6189795f0f8b007b9fda6c8
Jake Bladt <jake@ambulnz.com>
2017-06-02 17:15:25 -0400

    get header

--- balance01/valuate.py
+++ balance01/valuate.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 import csv
 
-f = open('sample.csv', 'rb')
+f = open('./sample.csv', 'rb')
 reader = csv.reader(f)
-for row in reader:
-  print row
+rownum = 0
+header = []
 
+for row in reader:
+  if rownum == 0:
+    header = row
+  else:
+    print row 
+  rownum += 1
+  
 f.close()
