f197bb9ea4c1161c65663be46d94835b0c86e89d
Jake Bladt <jake@ambulnz.com>
2017-06-02 18:02:01 -0400

    +last names

--- locn/penerate.py
+++ locn/penerate.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python
+
+last_names = ['Smith', 'Johnson', 'Williams', 'Jones', 'Brown', 'Davis', 'Miller', 'Wilson', \
+  'Moore', 'Taylor', 'Anderson', 'Thomas', 'Jackson', 'White', 'Harris', 'Martin', 'Thompson', \
+  'Garcia', 'Martinez', 'Robinson']
+
+
