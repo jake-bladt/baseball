2a5e1e6db3971ca3e2fa38c5b890f9ae8b270ff2
Jake Bladt <jake@ambulnz.com>
2017-06-02 17:27:09 -0400

    +average

--- balance01/valuate.py
+++ balance01/valuate.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import csv
 
+def average(list):
+  return reduce(lambda x, y: x + y, list) / len(list)
+
 class Batter:
   def __init__(self, name, hrs, runs, steals):
     self.name = name
@@ -11,6 +14,14 @@ class Batter:
   @staticmethod
   def from_csv_row(row):
     return Batter(row[0], int(row[1]), int(row[2]), int(row[3]))
+
+class Universe:
+  def __init__(self, players):
+    self.players = players
+
+  def calculate_averages():
+
+
 
 f = open('./sample.csv', 'rb')
 reader = csv.reader(f)
