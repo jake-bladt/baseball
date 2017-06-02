2ab98e5f21884f9fd014fe71e157dc1b5a2f8a8c
Jake Bladt <jake@ambulnz.com>
2017-06-02 17:21:44 -0400

    create players

--- balance01/valuate.py
+++ balance01/valuate.py
@@ -8,16 +8,23 @@ class Batter:
     self.runs = runs
     self.stolenBases = steals
 
+  @staticmethod
+  def from_csv_row(row):
+    return Batter(row[0], int(row[1]), int(row[2]), int(row[3]))
+
 f = open('./sample.csv', 'rb')
 reader = csv.reader(f)
 rownum = 0
 header = []
+players = []
 
 for row in reader:
   if rownum == 0:
     header = row
   else:
-    print row 
+    players.push(Batter.from_csv_row(row)) 
   rownum += 1
+
+print str(len(players)) + ' players.'
   
 f.close()
