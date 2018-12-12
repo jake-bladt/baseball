4197976fcdbea62758fa11910e28a88b25049b27
Jake Bladt <jake@jakebladt.com>
2018-12-11 22:24:45 -0500

    +slightly less smoky test

--- parse_boxes/tests/TestBscoreParser.py
+++ parse_boxes/tests/TestBscoreParser.py
@@ -1,7 +1,13 @@
 import unittest
 
+def add_one(i):
+    return i + 1
+
 class BoxScoreParserTestCase(unittest.TestCase):
     """Tests for BoxScoreParser class"""
 
     def test_smoke_test(self):
         self.assertTrue(True)
+
+    def test_add_one(self):
+        self.assertEqual(add_one(0), 1)
